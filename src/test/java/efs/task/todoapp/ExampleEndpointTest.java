57f25350ad8eb0cfe2c312cf3459b0004e6f8526
Jamabaiz <bartula.jan@gmail.com>
2021-06-06 19:20:19 +0200

    Basic testing

--- src/test/java/efs/task/todoapp/ExampleEndpointTest.java
+++ src/test/java/efs/task/todoapp/ExampleEndpointTest.java
@@ -2,15 +2,21 @@ package efs.task.todoapp;
 
 import efs.task.todoapp.util.ToDoServerExtension;
 import efs.task.todoapp.web.HttpCode;
+import efs.task.todoapp.web.HttpMethode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Flow;
 
 import static java.net.http.HttpResponse.BodyHandlers.ofString;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(ToDoServerExtension.class)
 class ExampleEndpointTest {
 
-    public static final String TODO_APP_PATH = "http://localhost:8080/todo/";
+    public static final String TODO_APP_PATH = "http://localhost:8080";
 
     private HttpClient httpClient;
 
@@ -32,7 +38,7 @@ class ExampleEndpointTest {
     void shouldReturnNotFoundStatusForUnhandledPaths() throws IOException, InterruptedException {
         //given
         var httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(TODO_APP_PATH + "non/existing/endpoint"))
+                .uri(URI.create(TODO_APP_PATH + "/todo/non/existing/endpoint"))
                 .GET()
                 .build();
 
@@ -41,5 +47,68 @@ class ExampleEndpointTest {
 
         //then
         assertThat(httpResponse.statusCode()).as("Response status code").isEqualTo(HttpCode.NotFound.getrCode());
+    }
+
+    @ParameterizedTest(name = "{index}: path={0}, methode={1}")
+    @CsvSource({"/todo/user,POST", "/todo/task,POST", "/todo/task,GET"
+            , "/todo/task/1,GET", "/todo/task/1,PUT", "/todo/task/1,DELETE"})
+    @Timeout(1)
+    void shouldReturnOKStatusForPaths(String path, String str_methode) throws IOException, InterruptedException {
+        HttpMethode methode = HttpMethode.valueOf(str_methode);
+
+        HttpRequest httpRequest;
+
+
+        System.out.println("Execute: " + path + " | " + str_methode);
+        switch (methode) {
+            case GET: {
+                httpRequest = HttpRequest.newBuilder()
+                        .uri(URI.create(TODO_APP_PATH + path))
+                        .GET()
+                        .build();
+                break;
+            }
+            case PUT: {
+                httpRequest = HttpRequest.newBuilder()
+                        .uri(URI.create(TODO_APP_PATH + path))
+                        .header("Content-Type", "application/json")
+                        .PUT(HttpRequest.BodyPublishers.ofString(""))
+                        .build();
+                break;
+            }
+            case POST: {
+
+                httpRequest = HttpRequest.newBuilder()
+                        .uri(URI.create(TODO_APP_PATH + path))
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(""))
+                        .build();
+                break;
+            }
+            case DELETE: {
+                httpRequest = HttpRequest.newBuilder()
+                        .uri(URI.create(TODO_APP_PATH + path))
+                        .header("Content-Type", "application/json")
+                        .DELETE()
+                        .build();
+                break;
+            }
+            default: {
+                assertThat(true).isEqualTo(false);
+                httpRequest = HttpRequest.newBuilder()
+                        .uri(URI.create(TODO_APP_PATH + path))
+                        .GET()
+                        .build();
+                break;
+            }
+        }
+
+        var httpResponse = httpClient.send(httpRequest, ofString());
+
+        System.out.println(httpResponse.body());
+        //then
+        assertThat(httpResponse.statusCode()).as("Response status code").isEqualTo(HttpCode.OK.getrCode());
+
+
     }
 }

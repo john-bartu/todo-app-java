10f331e6b9706f1852875e6f6c396df090329868
Jamabaiz <bartula.jan@gmail.com>
2021-06-06 17:23:02 +0200

    Basic entities implementation

--- src/main/java/efs/task/todoapp/repository/TaskEntity.java
+++ src/main/java/efs/task/todoapp/repository/TaskEntity.java
@@ -1,4 +1,35 @@
 package efs.task.todoapp.repository;
 
+import java.util.UUID;
+
 public class TaskEntity {
+    UUID id;
+    String description;
+    String due;
+
+    public TaskEntity(String description, String due) {
+        id = UUID.randomUUID();
+        this.description = description;
+        this.due = due;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public void setDue(String due) {
+        this.due = due;
+    }
 }
--- src/main/java/efs/task/todoapp/repository/UserEntity.java
+++ src/main/java/efs/task/todoapp/repository/UserEntity.java
@@ -1,4 +1,21 @@
 package efs.task.todoapp.repository;
 
+import java.util.UUID;
+
 public class UserEntity {
+    String username;
+    String password;
+
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
